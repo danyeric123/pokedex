0135a859ae3a2aa753fd31a35e37b373bf925362
danyeric123 <dnagarpowers@gmail.com>
2024-07-07 16:00:44 -0400

    complete project

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,67 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
+import { Pokemon, PokemonAPIResponse } from "./interfaces";
+import PokedexTable from "./components/PokedexTable";
+import PokemonTypeSelection from "./components/PokemonTypeSelection";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [selectedType, setSelectedType] = useState<string>("");
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+
+  useEffect(() => {
+    fetchPokemons(page);
+  }
+  , [page]);
+
+  
+  const fetchPokemons = async (page: number = 1) => {
+    setLoading(true);
+    try {
+      const requests = [];
+      const offset = (page - 1) * 151;
+      for (let id = 1 + offset; id <= 151+offset; id++) {
+        requests.push(axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`));
+      }
+      const responses = await axios.all(requests);
+      const pokemonsAPI: PokemonAPIResponse[] = responses.map(response => response.data);
+      const pokemons = pokemonsAPI.map(transformPokemon);
+      setPokemons(pokemons);
+    } catch (error) {
+      console.error("Failed to fetch pokemons:", error);
+      // Handle error appropriately
+    }
+    setLoading(false);
+    setPage(page);
+  };
+
+  const transformPokemon = (pokemon: PokemonAPIResponse): Pokemon => {
+    return {
+      name: pokemon.name,
+      id: pokemon.id,
+      types: pokemon.types.map(type => type.type.name),
+      sprites: {
+        front_default: pokemon.sprites.front_default,
+        front_shiny: pokemon.sprites.front_shiny
+      }
+    };
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <h1>Pokedex</h1>
+      <button onClick={()=>fetchPokemons(page-1)} disabled={loading}>
+        {loading ? "Loading..." : "Previous Page"}
+      </button>
+      <button onClick={()=>fetchPokemons(page+1)} disabled={loading}>
+        {loading ? "Loading..." : "Next Page"}
+      </button>
+      <PokemonTypeSelection selectType={setSelectedType} selectedType={selectedType} />
+      <PokedexTable pokemons={pokemons.filter(pokemon => !selectedType || pokemon.types.includes(selectedType))} />
+    </div>
+    
   );
 }
 
